8930e519313f4e12dc82dcbd04584a59949abdf1
odrozdova <sovunyaolya@gmail.com>
2019-07-09 18:16:05 +0300

    Endpoint for adding new themes

--- rest/src/main/java/com/gotogether/config/SecurityConfig.java
+++ rest/src/main/java/com/gotogether/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity security) throws Exception
     {
         security.httpBasic().disable();
+        security.cors().and().csrf().disable();
     }
 
 }
